21381e01a27ac20db780610eff6ecd8f25cff9f5
Usman Shabbir <usman.shabbir@digital.hmrc.gov.uk>
2018-06-25 12:31:16 +0100

    LockRepo

---
+++ app/uk/gov/hmrc/customs/notification/repo/LockRepo.scala
@@ -0,0 +1,21 @@
+package uk.gov.hmrc.customs.notification.repo
+
+import org.joda.time.DateTime
+import uk.gov.hmrc.customs.notification.domain.{ClientNotification, ClientSubscriptionId}
+
+import scala.concurrent.Future
+import scala.concurrent.duration.Duration
+
+/**
+  * Created by dev on 25/06/2018.
+  */
+trait LockRepo {
+
+
+  def lock(csid: ClientSubscriptionId): Future[Boolean]
+
+  def release(csid: ClientSubscriptionId): Future[Unit]
+
+  // if it returns false, stop processing the client, abort abort abort
+  def refreshLock(csid: ClientSubscriptionId, duration: Duration): Future[Boolean]
+}
