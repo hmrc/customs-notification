ada4fcd70bab6744191bb03177b4cb21dd54eb0c
avinder.bahra <avinder.bahra@digital.hmrc.gov.uk>
2018-07-04 16:39:38 +0100

    CDD-1566: Basic fetch processing implemented for ClientWorkerImpl.

--- app/uk/gov/hmrc/customs/notification/services/ClientWorker.scala
+++ app/uk/gov/hmrc/customs/notification/services/ClientWorker.scala
@@ -17,13 +17,11 @@
 package uk.gov.hmrc.customs.notification.services
 
 import uk.gov.hmrc.customs.notification.domain.ClientSubscriptionId
+import uk.gov.hmrc.customs.notification.repo.LockOwnerId
 
 import scala.concurrent.Future
 
-/**
-  * Created by dev on 25/06/2018.
-  */
 trait ClientWorker {
 
-  def processNotificationsFor(csid: ClientSubscriptionId): Future[Unit]
+  def processNotificationsFor(csid: ClientSubscriptionId, lockOwnerId: LockOwnerId): Future[Unit]
 }
