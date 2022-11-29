f69aed5dfc577e135b395be824b280d86c515457
Ian Wood <86672642+IanWood-HMRC@users.noreply.github.com>
2022-11-29 11:59:52 +0000

    DCWL-851 verify logger

--- test/unit/connectors/FieldsIdMapperHotFixSpec.scala
+++ test/unit/connectors/FieldsIdMapperHotFixSpec.scala
@@ -16,9 +16,12 @@
 
 package unit.connectors
 
-import uk.gov.hmrc.customs.notification.controllers.{FieldsIdMapperHotFix, RequestMetaData}
-import uk.gov.hmrc.customs.notification.logging.NotificationLogger
+import org.mockito.ArgumentMatchers.any
 import org.scalatestplus.mockito.MockitoSugar
+import uk.gov.hmrc.customs.notification.controllers.{FieldsIdMapperHotFix, RequestMetaData}
+import uk.gov.hmrc.customs.notification.domain.HasId
+import uk.gov.hmrc.customs.notification.logging.NotificationLogger
+import util.MockitoPassByNameHelper.PassByNameVerifier
 import util.UnitSpec
 
 class FieldsIdMapperHotFixSpec extends UnitSpec with MockitoSugar {
@@ -26,7 +29,15 @@ class FieldsIdMapperHotFixSpec extends UnitSpec with MockitoSugar {
   trait TestSetup {
     val logger = mock[NotificationLogger]
     implicit val md: RequestMetaData = mock[RequestMetaData]
-    val h = new FieldsIdMapperHotFix(logger)
+    val fieldsIdMapperHotFix = new FieldsIdMapperHotFix(logger)
+  }
+
+  private def logVerifier(mockLogger: NotificationLogger, logLevel: String, logText: String): Unit = {
+    println(s"[$logLevel] [$logText]")
+    PassByNameVerifier(mockLogger, logLevel)
+      .withByNameParam(logText)
+      .withParamMatcher(any[HasId])
+      .verify()
   }
 
   "FieldsIdMapperHotFix" should {
@@ -35,7 +46,8 @@ class FieldsIdMapperHotFixSpec extends UnitSpec with MockitoSugar {
       new TestSetup {
         val oldOne = "c86521a1-3bc3-4408-8ba4-4f51acaeb4d9"
         val newOne = FieldsIdMapperHotFix.workingFieldsId
-        assert(newOne == h.translate(oldOne))
+        assert(newOne == fieldsIdMapperHotFix.translate(oldOne))
+        logVerifier(logger, "warn", s"FieldsIdMapperHotFix: translating fieldsId [$oldOne] to [$newOne].")
       }
     }
 
@@ -43,7 +55,8 @@ class FieldsIdMapperHotFixSpec extends UnitSpec with MockitoSugar {
       new TestSetup {
         val oldOne = "f964448d-7cf0-444e-9027-172162235dbf"
         val newOne = FieldsIdMapperHotFix.workingFieldsId
-        assert(newOne == h.translate(oldOne))
+        assert(newOne == fieldsIdMapperHotFix.translate(oldOne))
+        logVerifier(logger, "warn", s"FieldsIdMapperHotFix: translating fieldsId [$oldOne] to [$newOne].")
       }
     }
 
@@ -51,7 +64,8 @@ class FieldsIdMapperHotFixSpec extends UnitSpec with MockitoSugar {
       new TestSetup {
         val oldOne = "8a2e1a95-6240-4256-9439-2ee0c59a16d6"
         val newOne = FieldsIdMapperHotFix.workingFieldsId
-        assert(newOne == h.translate(oldOne))
+        assert(newOne == fieldsIdMapperHotFix.translate(oldOne))
+        logVerifier(logger, "warn", s"FieldsIdMapperHotFix: translating fieldsId [$oldOne] to [$newOne].")
       }
     }
 
@@ -59,7 +73,7 @@ class FieldsIdMapperHotFixSpec extends UnitSpec with MockitoSugar {
       new TestSetup {
         val oldOne = "anyOtherString"
         val newOne = oldOne
-        assert(newOne == h.translate(oldOne))
+        assert(newOne == fieldsIdMapperHotFix.translate(oldOne))
       }
     }
   }
