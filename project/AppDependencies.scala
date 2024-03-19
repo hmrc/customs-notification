af5e10191e11806f78c535cbdf5402355da0c90f
Ian Wood <86672642+IanWood-HMRC@users.noreply.github.com>
2024-03-19 17:25:19 +0000

    DCWL-1834 Formatting

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,19 +6,19 @@ object AppDependencies {
   private val mongoVersion = "1.8.0"
 
   val compile = Seq(
-    "uk.gov.hmrc.mongo"        %% "hmrc-mongo-work-item-repo-play-28"   % mongoVersion,
-    "uk.gov.hmrc"              %% "bootstrap-backend-play-28"           % "8.0.0",
-    "org.typelevel"            %% "cats-core"                           % "2.9.0"
+    "uk.gov.hmrc.mongo"            %% "hmrc-mongo-work-item-repo-play-28"      % mongoVersion,
+    "uk.gov.hmrc"                  %% "bootstrap-backend-play-28"              % "8.0.0",
+    "org.typelevel"                %% "cats-core"                              % "2.9.0"
   )
 
   val test = Seq(
     "org.scalatestplus.play"       %% "scalatestplus-play"      % "5.1.0"      % testScope,
-    "com.github.tomakehurst"        % "wiremock-standalone"     % "2.27.2"      % testScope,
+    "com.github.tomakehurst"        % "wiremock-standalone"     % "2.27.2"     % testScope,
     "org.scalatestplus"            %% "scalatestplus-mockito"   % "1.0.0-M2"   % testScope,
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-test-play-28" % mongoVersion % testScope,
     "com.vladsch.flexmark"          % "flexmark-all"            % "0.35.10"    % testScope,
     "uk.gov.hmrc"                  %% "bootstrap-test-play-28"  % "8.0.0"      % testScope,
-    "org.mockito"                   % "mockito-core"            % "5.3.1"     % testScope,
+    "org.mockito"                   % "mockito-core"            % "5.3.1"      % testScope,
     "com.fasterxml.jackson.module" %% "jackson-module-scala"    % "2.17.0"     % testScope
   )
 }
